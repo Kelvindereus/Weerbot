fd017357abd6b9cc643353ba2e319c400cc01054
Kelvin de Reus <k_dereus@outlook.com>
2022-10-26 23:42:43 +0200

    Update GithubScript

--- GItHubAction/GitHubPteroScript.py
+++ GItHubAction/GitHubPteroScript.py
@@ -1,3 +1,4 @@
+# Importing the modules
 from pydactyl import PterodactylClient
 import os
 from dotenv import load_dotenv
@@ -5,19 +6,14 @@ from dotenv import load_dotenv
 # Loading envirement keys
 load_dotenv()
 
-# Get Game_manager env keys
+# Get the gamemanager URL and API key
 gamemanager_url = os.getenv("gamemanager_ptero_url")
 gamemanager_client_password = os.getenv("client_api_key")
 
-# Create a client to connect to the panel and authenticate with your API key.
+# Create a client to connect to the panel by URL and authenticate with your API key
 api = PterodactylClient(gamemanager_url, gamemanager_client_password)
 
-# Get a list of all servers
-my_servers = api.client.servers.list_servers()
-
-# Get the second server
-srv_id = my_servers[5]["attributes"]["identifier"]
+SERVER_ID = "d431d685"
 
 # Rebooting the individual server
-api.client.servers.send_power_action(srv_id, "restart")
-
+api.client.servers.send_power_action(SERVER_ID, "restart")
